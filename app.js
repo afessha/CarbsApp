4b4abdaa448c7297ae1cd800b592cbb9f91276eb
Awet Fessha <awetkifle94@gmail.com>
2019-05-02 11:43:58 +0100

    serve home.html

--- app.js
+++ app.js
@@ -7,15 +7,16 @@ app.get("/", function(req, res) {
   console.log("The server is rrunning at port " + port);
 });
 
+//Route to handle the path (/home)
+app.get("/home", function(req, res) {
+  res.sendFile(path.join(__dirname + "/home.html"));
+});
+
 //Route to handle the path (/food)
 app.get("/food", function(req, res) {
   res.sendFile(path.join(__dirname + "/food.html"));
 });
 
-//Route to handle the path (/home)
-app.get("/home", function(req, res) {
-  res.send("home");
-});
 //Route to handle the path (/login)
 app.get("/login", function(req, res) {
   res.sendFile(path.join(__dirname + "/login.html"));

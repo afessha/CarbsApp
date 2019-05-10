3f1cb0102e4c215cf3668edf16568319495299ee
Awet Fessha <awetkifle94@gmail.com>
2019-05-10 11:59:55 +0100

    post data to mongodb

--- app.js
+++ app.js
@@ -2,33 +2,41 @@ var mongoclient = require("mongodb").MongoClient;
 var url =
   "mongodb+srv://awet:Shirba94@cluster0-ulesp.mongodb.net/carbsDB?retryWrites=true";
 var express = require("express");
+var bodyParser = require("body-parser");
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 var path = require("path");
 var app = express();
 var port = process.env.PORT || 8080;
 app.use(express.static("public"));
-app.use("/", function(req, res, next) {
+
+//posting
+app.post("/users", urlencodedParser, function(req, res) {
+  res.send("Data recieved");
+  console.log(req.body.firstname);
+  console.log(req.body.lastname);
+  console.log(req.body.email);
+  console.log(req.body.password);
+  console.log(req.body.confirm);
   mongoclient.connect(url, { useNewUrlParser: true }, function(err, db) {
     if (err) throw err;
-    console.log("Connection succesful");
-
+    console.log("succesfull connection");
     var database = db.db("carbsDB");
-    database.collection("users").findOne({}, function(err, result) {
+    var obj = {
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
+      email: req.body.email,
+      password: req.body.password,
+      confirm: req.body.confirm
+    };
+    database.collection("users").insertOne(obj, function(err, result) {
       if (err) throw err;
-      console.log(result.firstname + " " + result.lastname);
+      console.log("Data has been added");
     });
-
-    //adding new data to carbsDB database
-    var obj = { firstname: "Jane", lastname: "Jack", email: "jane@hotmail.com", password: "1234", confirm: "123" }
-    database.collection("users").insertOne(obj, function (err, result) {
-      if (err) throw err;
-      console.log("Document added to database");
-    })
   });
-  next();
 });
 app.get("/", function(req, res) {
   res.send("Hello my server is working");
-  console.log("The server is rrunning at port " + port);
+  console.log("The server is running at port " + port);
 });
 
 //Route to handle the path (/home)

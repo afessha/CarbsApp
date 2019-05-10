1aa39c8f2cca5e216f42301c46907194b024b37c
Awet Fessha <awetkifle94@gmail.com>
2019-05-10 10:21:49 +0100

    installed body-parser and insert a value

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
-var mongodb = require("mongodb").MongoClient;
-var url = "mongodb+srv://awet:Shirba94@cluster0-ulesp.mongodb.net/carbsDB?retryWrites=true";
+var mongoclient = require("mongodb").MongoClient;
+var url =
+  "mongodb+srv://awet:Shirba94@cluster0-ulesp.mongodb.net/carbsDB?retryWrites=true";
 var express = require("express");
 var path = require("path");
 var app = express();
 var port = process.env.PORT || 8080;
 app.use(express.static("public"));
-app.use("/", function (req,res) {
-  
-})
+app.use("/", function(req, res, next) {
+  mongoclient.connect(url, { useNewUrlParser: true }, function(err, db) {
+    if (err) throw err;
+    console.log("Connection succesful");
+
+    var database = db.db("carbsDB");
+    database.collection("users").findOne({}, function(err, result) {
+      if (err) throw err;
+      console.log(result.firstname + " " + result.lastname);
+    });
+
+    //adding new data to carbsDB database
+    var obj = { firstname: "Jane", lastname: "Jack", email: "jane@hotmail.com", password: "1234", confirm: "123" }
+    database.collection("users").insertOne(obj, function (err, result) {
+      if (err) throw err;
+      console.log("Document added to database");
+    })
+  });
+  next();
+});
 app.get("/", function(req, res) {
   res.send("Hello my server is working");
   console.log("The server is rrunning at port " + port);
